3b6b41ff04b3442871472c43b769d0138b890fdd
↝Élise☀︎ <eliseduverdier@gmail.com>
2021-05-29 23:56:31 +0200

    fix: <br> to <br />

--- templates/includes/nav.php
+++ templates/includes/nav.php
@@ -1,5 +1,5 @@
 <nav>
-    <h1>elementary <br>cellular <br>automata</h1>
+    <h1>elementary <br />cellular <br />automata</h1>
     <div>
         <div><a href="generator.php">generate</a> | <a href="list.php">list</a></div>
     </div>
